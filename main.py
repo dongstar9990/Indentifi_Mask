cadeadf605d2c5025080ffd5d8798091827f44f4
ADMIN <nguyendong.g6land@gmail.com>
2023-04-02 03:19:54 +0700

    day la chuong trinh cong tru nhan chia

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# chuong trinh cong tru nhan chia co ban
+
+x = int(input("nhap so thu 1 "))
+y = int(input("Nhap so thu 2 "))
+
+def cong(x , y):
+    c=x+y
+    print(c)
+
+def tru (x, y):
+    c = x - y
+    print(c)
+
+def nhan (x, y):
+    c = x * y
+    print(c)
+
+
+def chia (x, y):
+    c = x / y
+    print(c)
+
+
+cong(x,y)
+tru(x, y)
+nhan(x, y)
+chia(x, y)
